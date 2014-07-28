00a1c5c377a820237b57bcd74aa05578d1ca34e7
Asaf Shakarchi <asaf000@gmail.com>
2014-07-28 18:46:23 +0300

    Initial 0.1.0 version

---
+++ README.md
@@ -0,0 +1,12 @@
+## ember-cli-components
+
+This addon that adds `ember components` to the generated Ember CLI output (in `vendor.js`).
+
+### Installation / Usage
+
+```bash
+npm install --save-dev ember-cli-components
+```
+
+### References
+* [ember-components](https://github.com/indexiatech/ember-components)
---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var path = require('path');
+var fs   = require('fs');
+
+function EmberCLIEmberComponentsAjax(project) {
+  this.project = project;
+  this.name    = 'Ember CLI ember-components';
+}
+
+function unwatchedTree(dir) {
+  return {
+    read:    function() { return dir; },
+    cleanup: function() { }
+  };
+}
+
+EmberCLIEmberComponentsAjax.prototype.treeFor = function treeFor(name) {
+  if (name !== 'vendor') { return; }
+
+  var treePath =  path.join('node_modules', 'ember-cli-ember-components', 'node_modules');
+
+  return unwatchedTree(treePath);
+};
+
+EmberCLIEmberComponentsAjax.prototype.included = function included(app) {
+  this.app = app;
+  var options = this.app.options.emberComponentsOptions || {enabled: true};
+
+  if (options.enabled) {
+    this.app.import('vendor/ember-components/vendor/ic-styled/main.js');
+    this.app.import('vendor/ember-components/vendor/ember-utils/dist/globals/main.js');
+    this.app.import('vendor/ember-components/dist/globals/main.js');
+  }
+};
+
+module.exports = EmberCLIEmberComponentsAjax;
